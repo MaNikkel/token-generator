3b132ffad6b723c159f5dae42e6a52db448c4e10
Mathias Nikkel <mathiasnikkel@gmail.com>
2020-12-22 11:54:58 -0300

    .

--- src/modules/tokens/providers/UploadTokenFileProvider/implementations/GithubUploadProvider.ts
+++ src/modules/tokens/providers/UploadTokenFileProvider/implementations/GithubUploadProvider.ts
@@ -1,26 +1,27 @@
-import axios from 'axios';
+// import axios from 'axios';
 import IUploadTokenFileProvider from '../models/IUploadTokenFileProvider';
 
 export default class GithubUploadProvider implements IUploadTokenFileProvider {
   public async upload(tokens: any): Promise<void> {
-    console.log(tokens);
     try {
-      axios.post(
-        'https://api.github.com/repo/MaNikkel/token-generator/dispatches',
-        {
-          event_type: 'update-tokens',
-          client_payload: {
-            tokenFileName: 'latest.json',
-            tokens,
-          },
-        },
-        {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            accept: 'application/vnd.github.v3+json',
-          },
-        },
-      );
+      console.log(tokens);
+      // TODO: NOT WORKING
+      // axios.post(
+      //   'https://api.github.com/repo/MaNikkel/token-generator/dispatches',
+      //   {
+      //     event_type: 'update-tokens',
+      //     client_payload: {
+      //       tokenFileName: 'latest.json',
+      //       tokens,
+      //     },
+      //   },
+      //   {
+      //     headers: {
+      //       'Access-Control-Allow-Origin': '*',
+      //       accept: 'application/vnd.github.v3+json',
+      //     },
+      //   },
+      // );
     } catch (e) {
       console.log(e);
       throw new Error(e.message);
