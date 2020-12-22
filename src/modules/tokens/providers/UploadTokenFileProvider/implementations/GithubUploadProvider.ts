2eed76838a620a914ab16d4b590e0a475daf0f74
Mathias Nikkel <mathiasnikkel@gmail.com>
2020-12-22 11:53:27 -0300

    actions

--- src/modules/tokens/providers/UploadTokenFileProvider/implementations/GithubUploadProvider.ts
+++ src/modules/tokens/providers/UploadTokenFileProvider/implementations/GithubUploadProvider.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import IUploadTokenFileProvider from '../models/IUploadTokenFileProvider';
-// 5374a2bbd78ed434edee97484209e5ab82019cf8
+
 export default class GithubUploadProvider implements IUploadTokenFileProvider {
   public async upload(tokens: any): Promise<void> {
     console.log(tokens);
@@ -18,7 +18,6 @@ export default class GithubUploadProvider implements IUploadTokenFileProvider {
           headers: {
             'Access-Control-Allow-Origin': '*',
             accept: 'application/vnd.github.v3+json',
-            Authorization: `token 5374a2bbd78ed434edee97484209e5ab82019cf8`,
           },
         },
       );
